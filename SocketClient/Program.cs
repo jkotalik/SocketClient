fc88292b6ed53337105fb95d88160ad5fb128e7c
Justin Kotalik <jukotali@microsoft.com>
2017-10-14 13:28:18 -0700

    Still working on deadlock

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SocketClient
@@ -12,36 +13,58 @@ namespace SocketClient
         {
             try
             {
-                var ipAddress = IPAddress.Loopback;
-                var ipe = new IPEndPoint(ipAddress, 50690);
-                var socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                socket.Connect(ipe);
-                var testString = "hello world";
-                var request = $"POST / HTTP/1.0\r\n" +
-                        $"Host: localhost\r\n" +
-                        $"Content-Length: {testString.Length}" +
-                        "\r\n\r\n" +
-                        testString;
-                var bytesToSend = Encoding.ASCII.GetBytes(request);
-                var bytes = 0;
-                while ((bytes += socket.Send(bytesToSend, bytes, 1, SocketFlags.None)) < bytesToSend.Length)
+                while (true)
                 {
-                    Console.WriteLine($"Wrote a byte! {(char)bytesToSend[bytes - 1]}");
-                    await Task.Delay(100);
-                }
+                    // Setting up address stuff
+                    var ipAddress = IPAddress.Loopback;
+                    var ipe = new IPEndPoint(ipAddress, 50690);
+                    var socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ipe);
 
-                var buffer = new byte[1];
-                while (socket.Receive(buffer) != 0)
-                {
+                    var testString = "Hello the server should deadlock from this";
+                    var request = $"POST / HTTP/1.0\r\n" +
+                            $"Host: localhost\r\n" +
+                            $"Content-Length: {testString.Length}" +
+                            "\r\n\r\n";
+                    var bodyBytesToSend = Encoding.ASCII.GetBytes(testString);
+                    var requestBytes = Encoding.ASCII.GetBytes(request);
+
+                    var bytes = 0;
+                    while ((bytes += socket.Send(requestBytes, bytes, 1, SocketFlags.None)) < requestBytes.Length)
+                    {
+                    }
+                    Console.WriteLine("Done with headers, requesting bytes.");
+
+                    // Expect bytes back
+                    var buffer = new byte[1];
+                    // Should deadlock here if the server is not canceling the read.
+                    while (socket.Receive(buffer) != 0)
+                    {
+                        Console.Write((char)buffer[0]);
+                        Console.Out.Flush();
+                    }
+
                     Console.Write((char)buffer[0]);
                     Console.Out.Flush();
+
+                    bytes = 0;
+                    while ((bytes += socket.Send(bodyBytesToSend, bytes, 1, SocketFlags.None)) < bodyBytesToSend.Length)
+                    {
+                        Console.WriteLine($"Wrote a byte! {(char)bodyBytesToSend[bytes - 1]}");
+                    }
+
+                    while (socket.Receive(buffer) != 0)
+                    {
+                        Console.Write((char)buffer[0]);
+                        Console.Out.Flush();
+                    }
+                    Console.WriteLine("Done reading stream");
+                    Console.ReadLine();
                 }
-                Console.WriteLine("Done reading stream");
-                Console.ReadLine();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Connection Limit Hit: {ex.Message}");
+                Console.WriteLine($"Exception: {ex.Message}");
 
             }
         }
